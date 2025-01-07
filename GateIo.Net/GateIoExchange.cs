d76144812e48b6bd7e5ba3f6058a540b6496a970
Jkorf <jankorf91@gmail.com>
2025-01-07 14:29:55 +0100

    Updated CryptoExchange.Net version, added Type property to GateIoExchange class

--- GateIo.Net/GateIoExchange.cs
+++ GateIo.Net/GateIoExchange.cs
@@ -40,6 +40,11 @@ namespace GateIo.Net
             };
 
         /// <summary>
+        /// Type of exchange
+        /// </summary>
+        public static ExchangeType Type { get; } = ExchangeType.CEX;
+
+        /// <summary>
         /// Format a base and quote asset to a Gate.io recognized symbol 
         /// </summary>
         /// <param name="baseAsset">Base asset</param>
