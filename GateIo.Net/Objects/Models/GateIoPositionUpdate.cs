7ec7c9aab6ee352b0a998c92e8e2dccd5fa5b377
Alex1911 <tjxx_jgd@163.com>
2025-05-12 21:22:09 +0800

    cross_leverage_limit may decimals (#20)
    * cross_leverage_limit may decimals

--- GateIo.Net/Objects/Models/GateIoPositionUpdate.cs
+++ GateIo.Net/Objects/Models/GateIoPositionUpdate.cs
@@ -97,6 +97,6 @@ namespace GateIo.Net.Objects.Models
         /// Cross margin leverage
         /// </summary>
         [JsonPropertyName("cross_leverage_limit")]
-        public int? CrossLeverageLimit { get; set; }
+        public decimal? CrossLeverageLimit { get; set; }
     }
 }
