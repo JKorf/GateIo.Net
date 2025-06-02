ed7b927db70e650f5e4452be5d9cb9058cbf1804
Alex1911-Jiang <tjxx_jgd@163.com>
2025-06-02 23:46:51 +0800

    Add copy trading role

---
+++ GateIo.Net/Enums/CopyTradingRole.cs
@@ -0,0 +1,10 @@
+﻿namespace GateIo.Net.Enums
+{
+    public enum CopyTradingRole
+    {
+        OrdinaryUser = 0,
+        OrderLeader = 1,
+        Follower = 2,
+        OrderLeaderAndFollower = OrderLeader | Follower,
+    }
+}
