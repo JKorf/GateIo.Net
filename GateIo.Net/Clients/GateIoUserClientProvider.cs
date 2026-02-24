3b36fd0230e6aa4683795323c25c83ee2e4361b8
Jkorf <jankorf91@gmail.com>
2026-02-24 13:17:58 +0100

    Updated HttpClient registration logic

--- GateIo.Net/Clients/GateIoUserClientProvider.cs
+++ GateIo.Net/Clients/GateIoUserClientProvider.cs
@@ -42,6 +42,7 @@ namespace GateIo.Net.Clients
             IOptions<GateIoSocketOptions> socketOptions)
         {
             _httpClient = httpClient ?? new HttpClient();
+            _httpClient.Timeout = restOptions.Value.RequestTimeout;
             _loggerFactory = loggerFactory;
             _restOptions = restOptions;
             _socketOptions = socketOptions;
