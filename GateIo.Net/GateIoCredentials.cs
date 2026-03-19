7b305466de754c516b729e062e09adb1a40a54c0
Jkorf <jankorf91@gmail.com>
2026-03-19 16:36:17 +0100

    wip

--- GateIo.Net/GateIoCredentials.cs
+++ GateIo.Net/GateIoCredentials.cs
@@ -1,4 +1,5 @@
 ﻿using CryptoExchange.Net.Authentication;
+using System;
 
 namespace GateIo.Net
 {
@@ -8,12 +9,39 @@ namespace GateIo.Net
     public class GateIoCredentials : HMACCredential
     {
         /// <summary>
+        /// Create new credentials
+        /// </summary>
+        public GateIoCredentials() { }
+
+        /// <summary>
         /// Create new credentials providing only credentials in HMAC format
         /// </summary>
         /// <param name="key">API key</param>
         /// <param name="secret">API secret</param>
         public GateIoCredentials(string key, string secret) : base(key, secret)
         {
+        }
+
+        /// <summary>
+        /// Create new credentials providing HMAC credentials
+        /// </summary>
+        /// <param name="credential">HMAC credentials</param>
+        public GateIoCredentials(HMACCredential credential) : base(credential.Key, credential.Secret)
+        {
+        }
+
+        /// <summary>
+        /// Specify the HMAC credentials
+        /// </summary>
+        /// <param name="key">API key</param>
+        /// <param name="secret">API secret</param>
+        public GateIoCredentials WithHMAC(string key, string secret)
+        {
+            if (!string.IsNullOrEmpty(Key)) throw new InvalidOperationException("Credentials already set");
+
+            Key = key;
+            Secret = secret;
+            return this;
         }
     }
 }
