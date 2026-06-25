47027be0a91fcaff890fad961fb97b50ccc2c3c9
Alex1911-Jiang <tjxx_jgd@163.com>
2026-06-25 12:53:39 +0800

    fix query position

--- GateIo.Net/Objects/Models/GateIoPosition.cs
+++ GateIo.Net/Objects/Models/GateIoPosition.cs
@@ -25,7 +25,7 @@ namespace GateIo.Net.Objects.Models
         /// ["<c>size</c>"] Position size
         /// </summary>
         [JsonPropertyName("size")]
-        public long Size { get; set; }
+        public decimal Size { get; set; }
         /// <summary>
         /// ["<c>trade_max_size</c>"] Maximum position
         /// </summary>
