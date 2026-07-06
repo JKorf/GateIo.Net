78bea753a35668a1dee1d69db0bdf18ffb77fb80
Jkorf <jankorf91@gmail.com>
2026-07-06 14:57:30 +0200

    Updated GateIoPerpTriggerOrder model with missing properties

--- GateIo.Net/Objects/Models/GateIoPerpTriggerOrder.cs
+++ GateIo.Net/Objects/Models/GateIoPerpTriggerOrder.cs
@@ -32,6 +32,11 @@ namespace GateIo.Net.Objects.Models
         [JsonPropertyName("finish_time")]
         public DateTime? FinishTime { get; set; }
         /// <summary>
+        /// ["<c>trigger_time</c>"] Trigger time
+        /// </summary>
+        [JsonPropertyName("trigger_time")]
+        public DateTime? TriggerTime { get; set; }
+        /// <summary>
         /// ["<c>trade_id</c>"] Trade id
         /// </summary>
         [JsonPropertyName("trade_id")]
@@ -52,10 +57,80 @@ namespace GateIo.Net.Objects.Models
         [JsonPropertyName("reason")]
         public string? Reason { get; set; }
         /// <summary>
+        /// ["<c>reason_type</c>"] Reason type
+        /// </summary>
+        [JsonPropertyName("reason_type")]
+        public string? ReasonType { get; set; }
+        /// <summary>
         /// ["<c>order_type</c>"] Order type
         /// </summary>
         [JsonPropertyName("order_type")]
         public TriggerOrderType? OrderType { get; set; }
+        /// <summary>
+        /// ["<c>is_stop_order</c>"] Is stop order
+        /// </summary>
+        [JsonPropertyName("is_stop_order")]
+        public bool IsStopOrder { get; set; }
+        /// <summary>
+        /// ["<c>in_dual_mode</c>"] Is in hedge mode
+        /// </summary>
+        [JsonPropertyName("in_dual_mode")]
+        public bool InDualMode { get; set; }
+        /// <summary>
+        /// ["<c>me_order_id</c>"] Corresponding order ID for order take-profit/stop-loss orders
+        /// </summary>
+        [JsonPropertyName("me_order_id")]
+        public long? TpslCorrespondingOrderId { get; set; }
+        /// <summary>
+        /// ["<c>parent_id</c>"] Parent order id
+        /// </summary>
+        [JsonPropertyName("parent_id")]
+        public long? ParentId { get; set; }
+        /// <summary>
+        /// ["<c>stop_profit_price</c>"] Stop profit price
+        /// </summary>
+        [JsonPropertyName("stop_profit_price")]
+        public decimal? StopProfitPrice { get; set; }
+        /// <summary>
+        /// ["<c>stop_loss_price</c>"] Stop loss price
+        /// </summary>
+        [JsonPropertyName("stop_loss_price")]
+        public decimal? StopLossPrice { get; set; }
+        /// <summary>
+        /// ["<c>direction</c>"] Position side
+        /// </summary>
+        [JsonPropertyName("direction")]
+        public PositionSide? PositionSide { get; set; }
+        /// <summary>
+        /// ["<c>filled_size</c>"] Quantity filled
+        /// </summary>
+        [JsonPropertyName("filled_size")]
+        public decimal? QuantityFilled { get; set; }
+        /// <summary>
+        /// ["<c>pos_margin_mode</c>"] Margin mode
+        /// </summary>
+        [JsonPropertyName("pos_margin_mode")]
+        public MarginMode? MarginMode { get; set; }
+        /// <summary>
+        /// ["<c>position_mode</c>"] Position mode
+        /// </summary>
+        [JsonPropertyName("position_mode")]
+        public PositionHoldMode? PositionMode { get; set; }
+        /// <summary>
+        /// ["<c>finish_as_text</c>"] Finish text
+        /// </summary>
+        [JsonPropertyName("finish_as_text")]
+        public string? FinishText { get; set; }
+        /// <summary>
+        /// ["<c>is_splitting</c>"] Is splitting
+        /// </summary>
+        [JsonPropertyName("is_splitting")]
+        public bool IsSplitting { get; set; }
+        /// <summary>
+        /// ["<c>text_output</c>"] Text output
+        /// </summary>
+        [JsonPropertyName("text_output")]
+        public string? TextOutput { get; set; }
         /// <summary>
         /// ["<c>initial</c>"] Order info
         /// </summary>
@@ -89,6 +164,11 @@ namespace GateIo.Net.Objects.Models
         /// </summary>
         [JsonPropertyName("rule")]
         public TriggerType TriggerType { get; set; }
+        /// <summary>
+        /// ["<c>strategy_type</c>"] Strategy type
+        /// </summary>
+        [JsonPropertyName("strategy_type")]
+        public int StrategyType { get; set; }
         /// <summary>
         /// ["<c>expiration</c>"] Expire time in seconds
         /// </summary>
@@ -142,5 +222,20 @@ namespace GateIo.Net.Objects.Models
         /// </summary>
         [JsonPropertyName("auto_size")]
         public CloseSide? CloseSide { get; set; }
+        /// <summary>
+        /// ["<c>iceberg</c>"] Iceberg quantity
+        /// </summary>
+        [JsonPropertyName("iceberg")]
+        public decimal? IcebergQuantity { get; set; }
+        /// <summary>
+        /// ["<c>amount</c>"] Amount
+        /// </summary>
+        [JsonPropertyName("amount")]
+        public decimal? Amount { get; set; }
+        /// <summary>
+        /// ["<c>iceberg_amount</c>"] Iceberg amount
+        /// </summary>
+        [JsonPropertyName("iceberg_amount")]
+        public decimal? IcebergAmount { get; set; }
     }
 }
