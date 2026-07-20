90c2946215d8bb76de8a093e4449c1f85224d0f2
Jkorf <jankorf91@gmail.com>
2026-07-20 14:25:05 +0200

    wip

--- GateIo.Net/Clients/FuturesApi/GateIoRestClientPerpetualFuturesApiShared.cs
+++ GateIo.Net/Clients/FuturesApi/GateIoRestClientPerpetualFuturesApiShared.cs
@@ -175,7 +175,7 @@ namespace GateIo.Net.Clients.FuturesApi
 
         #region Futures Symbol client
 
-        SharedSymbolCatalog? IFuturesSymbolRestClient.FuturesSymbolCatalog => ExchangeSymbolCache.GetSymbolCatalog(_topicId, EnvironmentName, null);
+        SharedSymbolCatalog? IFuturesSymbolRestClient.FuturesSymbolCatalog => ExchangeSymbolCache.GetSymbolCatalog(_exchangeName, _topicId, EnvironmentName, null);
         GetFuturesSymbolsOptions IFuturesSymbolRestClient.GetFuturesSymbolsOptions { get; } = new GetFuturesSymbolsOptions(_exchangeName, false)
         {
             RequiredExchangeParameters = new List<ParameterDescription>
