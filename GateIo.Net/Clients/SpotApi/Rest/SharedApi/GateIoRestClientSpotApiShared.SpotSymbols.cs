17ef84449a8428aedf714decf43cab28e2307f73
Jkorf <jankorf91@gmail.com>
2026-09-22 15:39:09 +0200

    wip

--- GateIo.Net/Clients/SpotApi/Rest/SharedApi/GateIoRestClientSpotApiShared.SpotSymbols.cs
+++ GateIo.Net/Clients/SpotApi/Rest/SharedApi/GateIoRestClientSpotApiShared.SpotSymbols.cs
@@ -16,7 +16,7 @@ namespace GateIo.Net.Clients.SpotApi
     {
         #region Get Spot Symbols
 
-        async Task<ICallResult<SharedSpotSymbol[]>> IGetSpotSymbols.GetSpotSymbolsAsync(GetSymbolsRequest request, CancellationToken ct)
+        async Task<IExchangeCallResult<SharedSpotSymbol[]>> IGetSpotSymbols.GetSpotSymbolsAsync(GetSymbolsRequest request, CancellationToken ct)
             => await GetSpotSymbolsAsync(request, ct).ConfigureAwait(false);
 
         public SharedSymbolCatalog? SpotSymbolCatalog => ExchangeSymbolCache.GetSymbolCatalog(_exchangeName, _topicId, _api.EnvironmentName, null);
